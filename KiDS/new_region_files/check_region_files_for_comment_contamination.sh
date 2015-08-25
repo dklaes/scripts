670510c87252679f316560da9d3f628d2179aa99
Dominik Klaes <dklaes@astro.uni-bonn.de>
2015-08-25 18:07:36 +0200

    "KiDS/new_region_files/check_region_files_for_comment_contamination.sh": - Added checking if the string 'POLYGON' is more than one time in one line.

--- KiDS/new_region_files/check_region_files_for_comment_contamination.sh
+++ KiDS/new_region_files/check_region_files_for_comment_contamination.sh
@@ -21,4 +21,10 @@ do
     if [ "${NUM4}" != "0" ]; then
       echo "'polygon' instead of 'POLYGON' found: ${FILENAME}"
     fi
+
+    NUM5=`sed 's/POLYGON/POLYGON\n/g' ${FILENAME} | grep -c POLYGON`
+    NUM6=`grep -v "#" ${FILENAME} | grep -c POLYGON`
+    if [ "${NUM5}" != "${NUM6}" ]; then
+      echo "Several 'POLYGON' in one line found: ${FILENAME}"
+    fi
 done
