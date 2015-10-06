1d7a39116d6bbf081d64dbc32060a4a535b9cf83
Thomas Erben <erben@roe.ac.uk>
2015-10-06 12:31:38 +0100

    "stats/KiDS_V0.5.7A_2015.10.06/": - Added stats directory for V0.5.7A, only input files.

---
+++ KiDS/stats/KiDS_V0.5.7A_2015.10.06/get_statistics.sh
@@ -0,0 +1,48 @@
+SURVEY=KIDS
+VERSION=V0.5.7A
+
+MD=/disk3/erben/KIDSCOLLAB_V0.5.7/
+
+for FILTER in r_SDSS
+do
+  rm mag_limit_individual_${FILTER}.txt
+  rm mag_limit_${FILTER}.txt
+
+
+  for POINTING in `ls -1 --color=none ${MD} | grep ${SURVEY}_`
+  do
+    ldactoasc -i ${MD}/${POINTING}/${FILTER}/precoadd_${VERSION}/cat/${VERSION}.cat \
+              -t STATS -k ${VERSION} IMAGENAME -b -s | awk '$1==1 {print $2}' \
+              > tmp_${FILTER}_$$
+
+    # Getting information for limiting magnitude.
+    while read IMAGE
+    do
+      ldactoasc -i ${MD}/${POINTING}/${FILTER}/precoadd_${VERSION}/cat/${IMAGE}.cat \
+                -t FIELDS -k SEXBKDEV ZP EXPTIME -b \
+                >> mag_limit_individual_${FILTER}.txt
+    done < tmp_${FILTER}_$$
+
+
+    ldactoasc -i ${MD}/${POINTING}/${FILTER}/postcoadd_${VERSION}/cats/*.swarp.cut_sex_ldac.cat \
+              -t FIELDS -k SEXBKDEV MAGZP -b \
+              >> mag_limit_${FILTER}.txt
+
+
+    # Getting information for sseing.
+    while read IMAGE
+    do
+      ldactoasc -i ${MD}/${POINTING}/${FILTER}/precoadd_${VERSION}/cat/${VERSION}.cat \
+                -t STATS -s -k IMAGENAME SEEING -b | grep ${IMAGE} | \
+                awk '{print $2}' >> seeing_individual_${FILTER}.txt
+    done < tmp_${FILTER}_$$
+
+    ldactoasc -i ${MD}/${POINTING}/${FILTER}/postcoadd_${VERSION}/cats/*.swarp.cut_sex_ldac.cat \
+              -t FIELDS -k SEEING -b \
+              >> seeing_${FILTER}.txt
+
+
+    rm tmp_${FILTER}_$$
+
+  done
+done
